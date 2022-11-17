fa7af83217f2d8e1666a28f7b53599cfb5994f28
Thais <thaissfurtado@gmail.com>
2022-11-17 14:47:55 -0300

    fim modulo 12

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  //projectId: '7kkmbh',
+  projectId: '7kkmbh',
   e2e: {
     "baseUrl": "http://lojaebac.ebaconline.art.br/"
   },
